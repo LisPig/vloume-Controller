b80ebcfc82f7ef7361c5b6966d515b15af7ceeac
LisPig <lsp1129133435@hotmail.com>
2023-09-21 23:41:31 +0800

    补充option页面js操作代码

--- option.js
+++ option.js
@@ -17,6 +17,30 @@ window.onload = function(){
          for (let content of contents) { 
             if (content.id === id) { 
                 content.classList.remove('hidden'); 
+                const siteList = document.getElementById('siteList');
+                if(content.id === "option1"){
+                    document.getElementById('siteList').innerHTML = '';
+                    chrome.storage.local.get('mediaList', (result) => {
+                        let resultMediaList =  [...new Set(result.mediaList)];
+                        for (let i=0; i<resultMediaList.length;i++) {
+                            const listItem = document.createElement('li');
+                            listItem.classList.add('flex', 'items-center', 'justify-between','my-4');
+                        
+                            listItem.innerHTML = `
+                            <div class="w-8 h-8 mr-4 flex-none ring-2 ring-offset-2 ring-gray-200 display flex align-items-center justify-content-center">
+                            <img  src="${resultMediaList[i].logo}" class="object-fit cover object-position 50% 50%"></img>
+                            </div>
+                            <div class="ml-4 flex-grow">
+                                <strong class="block mb-1">${resultMediaList[i].url}</strong>
+                                <input class="w-full" type="range" min="0" max="1" step="0.1" value="${resultMediaList[i].volume}" data-site="${resultMediaList[i].url}"> 
+                            </div>
+                            <span id="volumevalue" class="text-right w-10 mt-4">${resultMediaList[i].volume * 100}%</span>
+                            `;
+                        
+                            siteList.appendChild(listItem);
+                        }
+                    })
+                }
             } else { 
                 content.classList.add('hidden'); 
             } 
